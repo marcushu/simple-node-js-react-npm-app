5fe90a0901aa072aa9e8576d92dc0664c2a7cd3d
marcushurn <marcushurn@hotmail.com>
2024-05-20 19:20:35 -0700

    updating test and adding test stage

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+echo 'Im adding this line just to make it more realistic, MH...'
 echo 'The following "npm" command (if executed) installs the "cross-env"'
 echo 'dependency into the local "node_modules" directory, which will ultimately'
 echo 'be stored in the Jenkins home directory. As described in'
