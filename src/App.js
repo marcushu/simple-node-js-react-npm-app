ba05910af9e73c8e3f934c0c3eaa32b1ac241a3e
marcushurn <marcushurn@hotmail.com>
2024-05-20 18:59:12 -0700

    text changes for home

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <h3>Add this title and rebuild</h3>
+          <p>All of the rebuild stuff should happen with Jenkins....</p>
+        </main>
       </div>
     );
   }
